57f6d879c5428ffe026f30711192e561a965dc15
codermahbub20 <mkrefat5@gmail.com>
2025-01-17 20:50:16 +0600

    Refactor hard-coded sidebar items

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,32 +1,33 @@
-import { Layout, Menu, MenuProps } from "antd";
+import { Layout, Menu } from "antd";
 
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { adminSidebars } from "../../routes/routes.admins";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps["items"] = [
-  {
-    key: "Dashboard",
-    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-  },
-  {
-    key: "User Management",
-    label: "User Management",
-    children: [
-      {
-        key: "Create Student",
-        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
-      },
-      {
-        key: "Create Faculty",
-        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
-      },
-      {
-        key: "Create Admin",
-        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
-      },
-    ],
-  },
-];
+// const items: MenuProps["items"] = [
+//   {
+//     key: "Dashboard",
+//     label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
+//   },
+//   {
+//     key: "User Management",
+//     label: "User Management",
+//     children: [
+//       {
+//         key: "Create Student",
+//         label: <NavLink to="/admin/create-student">Create Student</NavLink>,
+//       },
+//       {
+//         key: "Create Faculty",
+//         label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+//       },
+//       {
+//         key: "Create Admin",
+//         label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
+//       },
+//     ],
+//   },
+// ];
 
 const MainLayout = () => {
   return (
@@ -57,7 +58,7 @@ const MainLayout = () => {
             theme="dark"
             mode="inline"
             defaultSelectedKeys={["4"]}
-            items={items}
+            items={adminSidebars}
           />
         </Sider>
         <Layout>
--- src/routes/routes.admins.tsx
+++ src/routes/routes.admins.tsx
@@ -1,38 +1,69 @@
 import { ReactNode } from "react";
-import App from "../App";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
+import AdminDashboard from "../pages/admin/AdminDashboard";
 
 type TRoute = { path: string; element: ReactNode };
+
+type TSidebarItems = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItems[];
+};
 
 export const adminPaths = [
   {
     name: "Dashboard",
-    path: "/admin/dashboard",
-    element: <App />,
+    path: "dashboard",
+    element: <AdminDashboard />,
   },
   {
     name: "User Management",
     children: [
       {
         name: "Create Student",
-        path: "/admin/create-student",
+        path: "create-student",
         element: <CreateStudent />,
       },
       {
         name: "Create Faculty",
-        path: "/admin/create-faculty",
+        path: "create-faculty",
         element: <CreateFaculty />,
       },
       {
         name: "Create Admin",
-        path: "/admin/create-admin",
+        path: "create-admin",
         element: <CreateAdmin />,
       },
     ],
   },
 ];
+
+export const adminSidebars = adminPaths.reduce((acc: TSidebarItems[], item) => {
+  if (item.path && item.name) {
+    acc.push({
+      key: item.name,
+      label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+    });
+  }
+
+  if (item.children) {
+    acc.push({
+      key: item.name,
+      label: item.name,
+      children: item.children.map((child) => {
+        return {
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        };
+      }),
+    });
+  }
+
+  return acc; // Ensures the accumulator is returned
+}, []); // Initialize the accumulator as an empty array
 
 export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
   if (item.path && item.element) {
