8b4c06f1b39056cdd860a165339cc45cbe064504
codermahbub20 <mkrefat5@gmail.com>
2025-01-21 20:37:43 +0600

    baseQueryWithRefreshToken

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -13,8 +13,14 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  return result;
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
